de96e0eab79cc612279a4f0b4dab234c2b22ed07
sinanthemainguy <sinanulbashir@gmail.com>
2025-04-16 14:23:10 +0500

    Module Types in Print Statement

---
+++ Vid_3_Modules/hello.py
@@ -0,0 +1,5 @@
+print("Hello there!")
+print("In Python, Modules are a piece of code that contain specific functions that programmers can use for their convenience.")
+print("\nThere are 2 types of Modules:")
+print("First one are 'In-built Modules'. These are already present in Python and don't need to be installed. e.g. calendar, cmath, and haslib")
+print("Second one are 'User-defined Modules' or 'External Modules'. They need to be installed. e.g. datetime, pandas, and tensorflow.")
